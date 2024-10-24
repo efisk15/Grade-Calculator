df29164770618d9f66edabd867b9f24c757ab1c7
judah benjamin <142185545+jubeju555@users.noreply.github.com>
2024-10-24 14:58:35 +0000

    new file:   .vscode/settings.json 	modified:   semester.h 	new file:   semster.cpp

--- semester.h
+++ semester.h
@@ -36,3 +36,5 @@ class GPA {
 };
 
 #endif
+
+
---
+++ semster.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<semester.h>
+
+using namespace std;
+
+
+GPA::GPA() {
+    vector<double> GPAs;
+
+}
+
+GPA::GPA(const GPA&){
+
+};
+
+GPA::~GPA(){
+
+}
